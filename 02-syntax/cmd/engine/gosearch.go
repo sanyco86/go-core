04b63eddf05a1c8ebef36cabc73480f8583fa376
Lutsko Michael <lutsko86@gmail.com>
2022-03-24 20:04:57 +0200

    02. HW syntax (#1)

---
+++ 02-syntax/cmd/engine/gosearch.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go-core/pkg/crawler"
+	"go-core/pkg/crawler/spider"
+	"log"
+	"strings"
+)
+
+const maxDepth = 3
+
+var urls = []string{"https://go.dev", "https://golang.org"}
+
+func main() {
+	q := flag.String("s", "", "Поиск")
+	flag.Parse()
+	if len(*q) == 0 {
+		flag.PrintDefaults()
+		return
+	}
+	fmt.Printf("Начался поиск по: %s...\n", *q)
+	pages := scan(urls)
+	for _, p := range pages {
+		if strings.Contains(strings.ToLower(p.Title), strings.ToLower(*q)) {
+			fmt.Printf("`%s` Найдено на: %s\n", p.Title, p.URL)
+		}
+	}
+}
+
+func scan(urls []string) []crawler.Document {
+	var result []crawler.Document
+	s := spider.New()
+	for _, url := range urls {
+		pages, err := s.Scan(url, maxDepth)
+		if err != nil {
+			log.Print(err)
+		}
+		result = append(result, pages...)
+	}
+	return result
+}
